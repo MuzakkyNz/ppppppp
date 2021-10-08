a0deda1fcf67c0745428e1b394998fbdabfc8279
MuzakkyNz <randubangoarek@gmail.com>
2021-10-08 13:01:59 +0700

    ngetes doang bang

---
+++ shop/allpayment.js
@@ -0,0 +1,31 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY Zakz Ganz
+
+*さ BANK*
+1. BRI 
+	ㅁ TUTUP
+		• TUTUP
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0857 3174 9158
+		• Mu***KY
+2. OVO
+	ㅁ 0857 3174 9158
+		• Mu***KY
+3. DANA
+	ㅁ 0857 3174 9158
+		• Mu***KY
+
+*さ PULSA*
+1. INDOSAT
+	ㅁ 0857 3174 9158
+2. THREE
+	ㅁ 0895 1437 7867
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
